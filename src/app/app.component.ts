1e326c9232ae7ea5a42bc65b3823e004c11bbc32
Evan Rettman <erettman@masonlive.gmu.edu>
2019-08-04 02:54:05 -0400

    Added game listing feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'very-cool-website';
+  games = [];
+
+  AddGame() {
+    const UserInput = document.getElementById('UserInput') as HTMLInputElement;
+    this.games.push(UserInput.value);
+    UserInput.value = '';
+  }
+
+  inputChange() {
+  }
 }
